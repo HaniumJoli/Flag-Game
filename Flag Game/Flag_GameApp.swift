d5e8878cddd8e472a5400817c0bb4e50cd3415eb
abir_zishan <rahman2007026@stud.kuet.ac.bd>
2024-11-09 19:11:17 +0600

    Initial Commit

---
+++ Flag Game/Flag_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flag_GameApp.swift
+//  Flag Game
+//
+//  Created by Abir Rahman on 9/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Flag_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
